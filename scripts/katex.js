b66b54200de40972b0374c2253a4f63a0d58bf3c
Takeshi Kaneko <target.ark@gmail.com>
2022-12-24 22:31:09 +0900

    Upgrade dependencies

--- scripts/katex.js
+++ scripts/katex.js
@@ -1,18 +1,13 @@
-hexo.extend.filter.register("markdown-it:renderer", function(md) {
-  md.use(
-    require("markdown-it-texmath"),
-    {
-      engine: require("katex"),
-      delimiters:"dollars",
-    }
-  );
+hexo.extend.filter.register("markdown-it:renderer", function (md) {
+  md.use(require("markdown-it-texmath"), {
+    engine: require("katex"),
+    delimiters: "dollars",
+  });
 });
 
-hexo.extend.filter.register('after_render:html', function (html) {
-  const linkTag = '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css" integrity="sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X" crossorigin="anonymous">'
+hexo.extend.filter.register("after_render:html", function (html) {
+  const linkTag =
+    '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.css" integrity="sha384-vKruj+a13U8yHIkAyGgK1J3ArTLzrFGBbBc0tDp4ad/EyewESeXE/Iv67Aj8gKZ0" crossorigin="anonymous">';
 
-  return html.replace(
-    /(<\/head>)/i,
-    linkTag + "$1",
-  );
+  return html.replace(/(<\/head>)/i, linkTag + "$1");
 });
