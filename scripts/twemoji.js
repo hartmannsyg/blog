e7c2b4f784b238d0cc391241b0e01e71af95c1da
Takeshi Kaneko <target.ark@gmail.com>
2023-01-14 23:29:38 +0900

    Fix broken twemoji links

--- scripts/twemoji.js
+++ scripts/twemoji.js
@@ -1,7 +1,10 @@
-hexo.extend.filter.register('markdown-it:renderer', function(md) {
-  var twemoji = require('twemoji');
+hexo.extend.filter.register("markdown-it:renderer", function (md) {
+  const twemoji = require("twemoji");
 
-  md.renderer.rules.emoji = function(token, idx) {
-    return twemoji.parse(token[idx].content);
+  md.renderer.rules.emoji = function (token, idx) {
+    return twemoji.parse(token[idx].content, {
+      // ref. https://github.com/twitter/twemoji/issues/580
+      base: "https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/",
+    });
   };
 });
